d123f5765d951f91955e1389d4b74831da928f84
javi <javipn85@gmail.com>
2025-01-12 17:42:41 +0100

    Cambien bien profe y delete de la estuctura Spreadsheet

--- Spreadsheet/Cell.py
+++
@@ -1,9 +0,0 @@
-from ContentHandler.Models.Content import Content
-
-class Cell:
-    def __init__(self, position: str, content: Content):
-        self.position = position
-        self.content = content
-
-    def get_content(self):
-        return self.content.getValue()
--- Spreadsheet/FileManager/SpreadsheetFileSaver.py
+++
@@ -1,9 +0,0 @@
-class SpreadsheetFileSaver:
-    def __init__(self, spreadsheet):
-        self.spreadsheet = spreadsheet
-
-    def save(self, file_path):
-        with open(file_path, 'w') as file:
-            for position, cell in self.spreadsheet.cells.items():
-                file.write(f"{position}: {cell.get_content()}\n")
-        print(f"Hoja de cálculo guardada en {file_path}")
